12243f18e1829cd40121dbec3e720a13e8697720
Liza Arora <liza.arora@outlook.com>
2022-05-02 09:50:20 +1000

    Complete

---
+++ index.js
@@ -0,0 +1,13 @@
+//function expression
+let divide = function() {
+    return 2000/100;
+}
+console.log(divide());
+
+//Arrow fucntion called square
+let square = (x) => x * x;
+console.log(square(5));
+
+//Arrow function called add
+let add = (a, b) => a + b;
+console.log(add(2, 3));
